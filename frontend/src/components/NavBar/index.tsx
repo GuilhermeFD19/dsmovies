873e6f369c799b5bd73e063f2deab415682776ff
GuilhermeFD19 <guii.doreto@gmail.com>
2022-03-09 12:48:27 -0300

    Navbar-new

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,23 +1,26 @@
-import { ReactComponent as GitHubIcon } from "assets/img/github.svg";
-import './styles.css';
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styles.css";
 
 function Navbar() {
-
-    return (
-        <header>
-            <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/GuilhermeFD19/dsmovies">
-                        <div className="dsmovie-contact-container">
-                            <GitHubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
-                        </div>
-                    </a>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a
+            href="https://github.com/GuilhermeFD19"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/GuilhermeFD19</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
