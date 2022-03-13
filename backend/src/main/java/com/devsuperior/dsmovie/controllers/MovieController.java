10a39826932903e878482c03598120aded207bd4
GuilhermeFD19 <guii.doreto@gmail.com>
2022-03-13 11:33:11 -0300

    movie

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -12,7 +12,7 @@ import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.services.MovieService;
 
 @RestController
-@RequestMapping(value = "/movies")
+@RequestMapping(value = "/movie")
 public class MovieController {
 	
 	@Autowired
